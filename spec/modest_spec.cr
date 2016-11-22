fb75e0bc3302f087e138634c6f595da672b327a2
Konstantin Makarchev <kostya27@gmail.com>
2016-11-23 00:24:55 +0300

    add more tests

--- spec/modest_spec.cr
+++ spec/modest_spec.cr
@@ -101,4 +101,49 @@ describe Modest do
     finder = parser.finder("")
     parser.css(finder).to_a.size.should eq 0
   end
+
+  it "integration test" do
+    html = <<-PAGE
+      <div>
+        <p id=p1>
+        <p id=p2 class=jo>
+        <p id=p3>
+          <a href="some.html" id=a1>link1</a>
+          <a href="some.png" id=a2>link2</a>
+        <div id=bla>
+          <p id=p4 class=jo>
+          <p id=p5 class=bu>
+          <p id=p6 class=jo>
+        </div>
+      </div>
+    PAGE
+
+    parser = Myhtml::Parser.new.parse(html)
+
+    # select all p nodes which id like `*p*`
+    parser.css("p[id*=p]").map(&.attribute_by("id")).to_a.should eq ["p1", "p2", "p3", "p4", "p5", "p6"]
+
+    # select all nodes with class "jo"
+    parser.css("p.jo").map(&.attribute_by("id")).to_a.should eq ["p2", "p4", "p6"]
+    parser.css(".jo").map(&.attribute_by("id")).to_a.should eq ["p2", "p4", "p6"]
+
+    # select odd child tag inside div, which not contain a
+    parser.css("div > :nth-child(2n+1):not(:has(a))").map(&.attribute_by("id")).to_a.should eq ["p1", "p4", "p6"]
+
+    # all elements with class=jo inside last div tag
+    parser.css("div").to_a.last.css(".jo").map(&.attribute_by("id")).to_a.should eq ["p4", "p6"]
+
+    # a element with href ends like .png
+    parser.css(%q{a[href$=".png"]}).map(&.attribute_by("id")).to_a.should eq ["a2"]
+
+    # find all a tags inside <p id=p3>, which href contain `html`
+    parser.css(%q{p[id=p3] > a[href*="html"]}).map(&.attribute_by("id")).to_a.should eq ["a1"]
+
+    # find all a tags inside <p id=p3>, which href contain `html` or ends_with `.png`
+    parser.css(%q{p[id=p3] > a:matches([href *= "html"], [href $= ".png"])}).map(&.attribute_by("id")).to_a.should eq ["a1", "a2"]
+
+    # create finder and use it in many places
+    finder = Myhtml::Parser.finder(".jo")
+    parser.css(finder).map(&.attribute_by("id")).to_a.should eq ["p2", "p4", "p6"]
+  end
 end
